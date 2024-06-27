db209780349e7b2c3195a56f6755a838d257c992
icra <alessandro@gessa.net>
2024-06-27 10:02:20 +0200

    Improve HEEX precompiling

--- lib/phoenix_slime/engine.ex
+++ lib/phoenix_slime/engine.ex
@@ -13,6 +13,15 @@ defmodule PhoenixSlime.Engine do
   defp read!(file_path) do
     file_path
     |> File.read!()
-    |> Slime.Renderer.precompile()
+    |> precompile(file_path)
   end
+
+  defp precompile(file, path) do
+    if is_sheex?(path),
+      do: Slime.Renderer.precompile_heex(file),
+      else: Slime.Renderer.precompile(file)
+  end
+
+  defp is_sheex?(path),
+    do: String.ends_with?(path, ".sheex")
 end
